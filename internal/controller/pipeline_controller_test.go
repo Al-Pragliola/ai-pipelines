82d9ffdff8a0f1cdbc79edb71f0c902cd4f7179b
Al-Pragliola <seth.pro@gmail.com>
2026-04-20 15:28:40 +0200

    fix: linting
    Signed-off-by: Al-Pragliola <seth.pro@gmail.com>

--- internal/controller/pipeline_controller_test.go
+++ internal/controller/pipeline_controller_test.go
@@ -21,9 +21,9 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
